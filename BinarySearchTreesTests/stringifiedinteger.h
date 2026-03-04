569b445b32fe9ad81d18eff7241d28aa4a15b0e5
Liviu Popa <liviucst@gmail.com>
2026-03-04 15:04:37 +0200

    Added description for the StringifiedInteger class

--- BinarySearchTreesTests/stringifiedinteger.h
+++ BinarySearchTreesTests/stringifiedinteger.h
@@ -2,6 +2,28 @@
 
 #include <string>
 
+/* This is a class that can be used for testing binary search trees.
+   It represents an integer that is rewritten using characters instead of numeric digits.
+
+   Each digit is mapped to a character as follows:
+   - 0: 'Z'
+   - 1: 'A'
+   - 2: 'B'
+   ...
+   - 9: 'I'
+
+   The - sign is mapped to the '_' character which is now placed at the end of the string sequence
+   - example 1: "BZA" is the equivalent for 201
+   - example 2: "GAB_" is the equivalent for -702
+
+   In addition to this the -infinite and +infinite values are being introduced:
+   - "N" for +infinite
+   - "N_" for -infinite
+
+   Any invalid string is mapped to either -infinite/+infinite depending on whether the last character in the sequence is
+   '_' or not.
+ */
+
 class StringifiedInteger
 {
 public:
